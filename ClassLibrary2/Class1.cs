340869b61a86fe499842bb393f47a3d232bceb5d
chitangchin <chitangb.chin@gmail.com>
2025-05-05 12:30:00 -0400

    Add project files.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+
+namespace ClassLibrary2
+{
+    /*
+     * Members
+     * - Fields
+     * - Constants (called when object of a class is created)
+     * - Properties 
+     * - Methods
+     * - Constructors
+     * - Events
+     * - Finalizers
+     * - Indexers
+     * - Operators
+     * - Nested Types
+     */
+    public class A 
+    {
+        int n1, n2;
+        public A()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+        public A(int i, int j)
+        {
+            n1 = i;
+            n2 = j;
+            Console.WriteLine("Parameterized Constructor");
+            Console.WriteLine(n1 + n2);
+        }
+    }
+
+    public class DerivedClassA : A
+    {
+        public DerivedClassA() : base()
+        {
+            Console.WriteLine("base class no param");
+        }
+        public DerivedClassA(int i, int j) : base(i, j)
+        {
+            Console.WriteLine("Derived class with param");
+            Console.WriteLine(i + j);
+        }
+    }
+    public class Person
+    {
+        public string? Name { get; set; } = null;
+        private readonly int _socialSecurity = 0;
+        public virtual void Speak()
+        {
+            Console.WriteLine("Woof");
+        }
+
+    }
+    public struct PointStruct(double x, double y)
+    {
+        public readonly double Sum { get; } = x + y;
+    }
+
+    public struct Distance(double x, double y)
+    {
+        public readonly double Magnitude { get; } = Math.Sqrt(x * x + y * y);
+        public readonly double Direction { get; } = Math.Atan2(x, y);
+        public readonly double Angle { get; } = Math.Atan2(y, x) * (180 / Math.PI);
+    }
+
+    public class WierdSum(double x, double y) : Person
+    { 
+        public override void Speak()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassLibrary2;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PointStruct point = new PointStruct(10, 12);
+            PointStruct point2 = point;
+
+            Console.WriteLine(point.Sum); // Output: 10
+            Console.WriteLine(point2.Sum); // Output: 99
+            Console.WriteLine();
+
+            Distance test = new Distance(3, 4);
+            Console.WriteLine(test.Magnitude); // Output: 5
+            Console.WriteLine(test.Angle); // Output: 0.9272952180016122
+            Console.WriteLine(test.Direction); // Output: 53.13010235415599
+            Console.WriteLine();
+
+            DerivedClassA derivedClassA = new DerivedClassA();
+            Console.WriteLine();
+            DerivedClassA derivedClassB = new DerivedClassA(10, 20);
+            Console.WriteLine();
+
+            Person p = new Person();
+            p.Name = "Brandon";
+            Person p2 = p;
+            p.Name = "Test";
+            Console.WriteLine(p.Name);
+
+        }
+    }
+}
